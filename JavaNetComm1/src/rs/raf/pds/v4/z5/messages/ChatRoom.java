41973a86a1177b40282095d114f6ed7dea531dc6
djokanovicivana <djokanovic.ivana.01@gmail.com>
2023-12-28 21:56:24 +0100

    creating ChatRoom.java

---
+++ JavaNetComm1/src/rs/raf/pds/v4/z5/messages/ChatRoom.java
@@ -0,0 +1,31 @@
+package rs.raf.pds.v4.z5.messages;
+
+import java.util.ArrayList;
+
+import com.esotericsoftware.kryonet.Connection;
+
+public class ChatRoom {
+	private String roomName;
+	private ArrayList<Connection> roomUsers;
+	private ArrayList<ChatMessage> roomMessages;
+	
+	 public ChatRoom(String roomName) {
+	        this.roomName = roomName;
+	        this.roomUsers = new ArrayList<>();
+	        this.roomMessages = new ArrayList<>();
+	    }
+
+	    public String getName() {
+	        return roomName;
+	    }
+
+	    public ArrayList<Connection> getUsers() {
+	        return roomUsers;
+	    }
+
+	    public ArrayList<ChatMessage> getMessages() {
+	        return roomMessages;
+	    }
+	
+
+}
