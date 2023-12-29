13e5797c767f41658c2dc9c97b57334cef50fce9
djokanovicivana <djokanovic.ivana.01@gmail.com>
2023-12-29 23:52:51 +0100

    JoinRoomRequest.java

---
+++ JavaNetComm1/src/rs/raf/pds/v4/z5/messages/JoinRoomRequest.java
@@ -0,0 +1,13 @@
+package rs.raf.pds.v4.z5.messages;
+
+public class JoinRoomRequest {
+	String roomName;
+	
+	public JoinRoomRequest() {
+		
+	}
+	public JoinRoomRequest(String roomName) {
+		this.roomName=roomName;
+	}
+
+}
