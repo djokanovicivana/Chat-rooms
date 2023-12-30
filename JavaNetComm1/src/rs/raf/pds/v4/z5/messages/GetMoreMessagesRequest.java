d3b581eb728d8f492955fd389248e8ef2d115031
djokanovicivana <djokanovic.ivana.01@gmail.com>
2023-12-30 14:49:37 +0100

    Update GetMoreMessagesRequest.java

--- JavaNetComm1/src/rs/raf/pds/v4/z5/messages/GetMoreMessagesRequest.java
+++ JavaNetComm1/src/rs/raf/pds/v4/z5/messages/GetMoreMessagesRequest.java
@@ -1,5 +1,16 @@
 package rs.raf.pds.v4.z5.messages;
 
 public class GetMoreMessagesRequest {
+	private String roomName;
+	
+	public GetMoreMessagesRequest() {
+		
+	}
+	public GetMoreMessagesRequest(String roomName) {
+		this.roomName=roomName;
+	}
+	public String getRoomName() {
+		return roomName;
+	}
 
 }
