c8ad95ed3501ca5170858dbcdab256eea4319698
djokanovicivana <djokanovic.ivana.01@gmail.com>
2023-12-30 14:39:19 +0100

    ListMessages.java

---
+++ JavaNetComm1/src/rs/raf/pds/v4/z5/messages/ListMessages.java
@@ -0,0 +1,14 @@
+package rs.raf.pds.v4.z5.messages;
+
+import java.util.ArrayList;
+
+public class ListMessages {
+	private ArrayList<ChatMessage> messages;
+	
+	public ListMessages() {}
+	
+	public ArrayList<ChatMessage> getMessages() {
+		return messages;
+	}
+
+}
